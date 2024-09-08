c55fe31f74b8f521e159095d4758b4c4e820d95c
ylu <123523291+ylu8888@users.noreply.github.com>
2024-09-07 22:01:53 -0400

    FINALLY DONE

--- nba.py
+++ nba.py
@@ -5,18 +5,14 @@ def main():
     """
     Main function that acts as the entry point of the program.
     """
-    #print("Welcome to the new Python program!")
-    # Your code starts here
+
     prob_func()
 
 def prob_func():
     N = 10000
-
-    probA = 0 #MIA up 3-0
     home_win = 0.75
     away_win = 0.25
     mia_wins = 0
-    comeback_wins = 0
     final_res = 0
 
     #g1 = bos
@@ -36,28 +32,14 @@ def prob_func():
         game6 = np.random.binomial(1, home_win)
         game7 = np.random.binomial(1, away_win)  
         
-        win_three = game1 + game2 + game3
-        if(win_three == 3): #if mia goes up 3-0
+        if(game1 + game2 + game3 == 3): #if mia goes up 3-0
             mia_wins += 1
 
-        lose_three = game4 + game5 + game6
-        if(lose_three == 0): #if mia loses games 4,5,6
-            if(game7 == 1): #if mia wins game 7
-                comeback_wins += 1 #P(losing first 3 and winning last) == 0.25 * 0.75 * 0.25 * 0.25 = 0.01172
-        
-        if(win_three == 3 and lose_three == 0): #if MIA wins 4-3, GIVEN that they went 3-0
-            if(game7 == 1):
-                final_res += 1
+            if(game4 + game5 + game6 == 0): #if MIA wins 4-3, GIVEN that they went 3-0
+                if(game7 == 1):
+                    final_res += 1
 
-    probA = mia_wins / N #0.25 * 0.25 * 0.75 = 0.047
-    #print(f"The probability of MIA going up 3-0 is {probA}") 
-
-    #probability MIA loses games 4-6 then wins game7, 0.25 * 0.75 * 0.25 * 0.25 = 0.0117
-    print(f"For N = {N}, the simulated value for part (e) is {comeback_wins / N}")
-
-    #0.25 * 0.25 * 0.75 * 0.25 * 0.75 * 0.25 * 0.25 = 0.00055
-    #print(f"The probability MIA wins 4-3, given they were up 3-0 is {final_res / N}") 
-
+    print(f"For N = {N}, the simulated value for part (e) is {final_res / mia_wins}")
 
 if __name__ == "__main__":
     main()
