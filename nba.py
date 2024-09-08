caca86a8bc023d63feeea928a5eade0004265e97
ylu <123523291+ylu8888@users.noreply.github.com>
2024-09-07 20:26:47 -0400

    FIN

--- nba.py
+++ nba.py
@@ -11,13 +11,14 @@ def main():
 
 def prob_func():
     N = 10000
-    res = 0
 
     probA = 0 #MIA up 3-0
-    probB = 0 #MIA wins 4-3
+    probBA = 0 #MIA wins 4-3
     home_win = 0.75
     away_win = 0.25
     mia_wins = 0
+    comeback_wins = 0
+    res = 0
 
     #g1 = bos
     #g2 = bos
@@ -35,32 +36,39 @@ def prob_func():
         if(game1 + game2 + game3 == 3): #if mia goes up 3-0
             mia_wins += 1
 
-    probA = mia_wins / N   
+            game4 = np.random.binomial(1, home_win) 
+            game5 = np.random.binomial(1, away_win)
+            game6 = np.random.binomial(1, home_win)
+            game7 = np.random.binomial(1, away_win) 
 
-    print(f"The probability of MIA going up 3-0 is {probA}")
-    
-    comeback_wins = 0
-  
-    for i in range(N): 
+            if(game4 + game5 + game6 == 0): #if mia loses games 4,5,6
+                if(game7 == 1): #if mia wins game 7
+                    #P(losing first 3 and winning last) == 0.25 * 0.75 * 0.25 * 0.25 = 0.01172
+                    comeback_wins += 1 
+
+    #print(f"The probability of MIA going up 3-0 is {probA}") 
+    probA = mia_wins / N   #this is the probability MIA goes up 3-0, 0.25 * 0.25 * 0.75, roughly 0.047
+
+    probBA = comeback_wins / N #this is the probability MIA loses games 4-6 then wins game7, roughly 0.01172
+
+    res = probBA / probA #this is the final solution, the prob MIA wins 4-3, given they were up 3-0
+
+    print(f"The probability MIA wins 4-3, given they were up 3-0 {res}")
+
+    for i in range(N):
         game4 = np.random.binomial(1, home_win) 
         game5 = np.random.binomial(1, away_win)
         game6 = np.random.binomial(1, home_win)
         game7 = np.random.binomial(1, away_win) 
 
-        if(game4 + game5 + game6 == 0):
-            if(game7 == 1):
-                comeback_wins += 1 
+        if(game4 + game5 + game6 == 0): #if mia loses games 4,5,6
+                if(game7 == 1): #if mia wins game 7
+                    #P(losing first 3 and winning last) == 0.25 * 0.75 * 0.25 * 0.25 = 0.01172
+                    comeback_wins += 1 
 
-    #P(losing first 3 and winning last) == 0.25 * 0.75 * 0.25 * 0.25
-    #the expected prob (mia_victory) should be 0.01172
-    mia_victory = comeback_wins / N #this is the probability MIA loses 3 then wins 1
-    print(f"The REAL probability of MIA losing game 4-6 and winning game 7 is {mia_victory}")
+    res_2e = comeback_wins / N
 
-    probB = mia_victory * probA
-    
-    print(f"The probability of MIA winning 4-3 is {probB}")
-    
-    #print(f"For N = {N}, the simulated value for part (e) is {mia_wins}")
+    print(f"For N = {N}, the simulated value for part (e) is {res_2e}")
 
 if __name__ == "__main__":
     main()
